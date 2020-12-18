e981be2cdfd630101c7387af32d3930c7fbf5915
Mikko Kuronen <mikko.p.kuronen@gmail.com>
2020-12-18 09:41:33 +0200

    Return zero for G when npoints = 1

--- src/G.jl
+++ src/G.jl
@@ -5,8 +5,12 @@ Return nearest neighbour distance distribution based on Kaplam-Meier estimator.
 """
 function Gkm(pp, r)
     xy = pp.data
+    if length(xy) == 1
+        return zeros(Float64, length(r))
+    end
     svec = @. SVector(getx(xy), gety(xy))
     tree = KDTree(svec)
+    # Take 2 nearest neighbors because, the first nearest neighbor is the point itself
     _, D = knn(tree, svec, 2, true)
     D = last.(D)
     B = [bdist(window(pp), x) for x in xy]
