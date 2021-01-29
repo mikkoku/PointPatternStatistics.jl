96bda6a5313db75967891763d089f65e170de0e8
mikkoku <mikko.p.kuronen@gmail.com>
2021-01-29 08:17:03 +0200

    reorganize tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,36 @@
 using PointPatternStatistics
 using Test
 include("reference_implementation.jl")
+@testset "PointPatternStatistics.jl" begin
+    window = (x=(-1.3,1), y=(0,1.6))
+    randu((a, b)) = a + rand() * (b-a)
+    randw(window) = (randu(window.x), randu(window.y))
+    xy = [randw(window) for _ in 1:300]
+    r = range(0, 0.25, length=512)
+    @testset "iterate" begin
+        @test collect(PointPattern(xy, window)) == xy
+    end
 
-window = (x=(-1.3,1), y=(0,1.6))
-randu((a, b)) = a + rand() * (b-a)
-randw(window) = (randu(window.x), randu(window.y))
-xy = [randw(window) for _ in 1:300]
-r = range(0, 0.25, length=512)
-est = pcf(PointPattern(xy, window), r)
-ref = pcfref(xy, window, r)
+    @testset "pcf" begin
+        est = pcf(PointPattern(xy, window), r)
+        ref = pcfref(xy, window, r)
+        @test sum(abs2, (est - ref)[2:end]) < 0.0001
+    end
 
-@test sum(abs2, (est - ref)[2:end]) < 0.0001
+    @testset "K" begin
+        est = Kest(PointPattern(xy, window), r)
+        ref = Kref(xy, window, r)
+        @test est ≈ ref
+    end
 
-est = Kest(PointPattern(xy, window), r)
-ref = Kref(xy, window, r)
-@test est ≈ ref
-
-include("F.jl")
-include("G.jl")
-#include("envelope.jl")
-include("SSI.jl")
+    @testset "F" begin
+        include("F.jl")
+    end
+    @testset "G" begin
+        include("G.jl")
+    end
+    #include("envelope.jl")
+    @testset "SSI" begin
+        include("SSI.jl")
+    end
+end
